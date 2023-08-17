c7d39459e93614a3f3fb22ec2c782b3423a9560f
alexmil-sk <alexmilweb@gmail.com>
2023-08-17 19:16:02 +0200

    Edit IntroPage

--- src/pages/IntroPage/IntroPage.jsx
+++ src/pages/IntroPage/IntroPage.jsx
@@ -81,11 +81,11 @@ export default function IntroComponent() {
 					<Link to="/ru/home"><span>RU</span></Link>
 				</motion.div>
 				<motion.div
-					initial={{ opacity: 0, x: -161 }}
-					animate={{ opacity: 1, x: 0 }}
+					initial={{ opacity: 0, y: 1000 }}
+					animate={{ opacity: 1, y: 0 }}
 					transition={{
-						duration: 1,
-						delay: 3
+						duration: 3,
+						delay: 1
 					}}
 				>
 					<Link to="/sk/home"><span>SK</span></Link>
